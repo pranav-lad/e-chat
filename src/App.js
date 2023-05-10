e937b33e638b92403f8baf97b4a1a3cedc1673f4
pranav-lad <pranav.lad20@pccoepune.org>
2023-05-10 15:30:02 +0530

    Starter Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Main from './main'
+import Navbar from './navbar'
+import Sidebar from './sidebar'
+import './css/Main.css'
+import './css/Navbar.css'
+import './css/Sidebar.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="container">
+        <Sidebar />
+        <Main />
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
---
+++ src/sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const sidebar = () => {
+  return (
+    <aside>
+      {/* sidebar content */}
+      Sidebar - Shreyash
+      Settings - Shreyash
+    </aside>
+  );
+};
+
+export default sidebar;
