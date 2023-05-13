1692ef2ad24a2e14e8594c6152de0310b67c94c1
pranav-lad <pranav.lad20@pccoepune.org>
2023-05-13 15:54:30 +0530

    Revert "Starter Code"
    This reverts commit e937b33e638b92403f8baf97b4a1a3cedc1673f4.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Main from './main'
-import Navbar from './navbar'
-import Sidebar from './sidebar'
-import './css/Main.css'
-import './css/Navbar.css'
-import './css/Sidebar.css'
-const App = () => {
+
+function App() {
   return (
-    <div>
-      <Navbar />
-      <div className="container">
-        <Sidebar />
-        <Main />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
-
+}
 
 export default App;
